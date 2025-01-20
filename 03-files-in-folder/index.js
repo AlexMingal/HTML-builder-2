90cee525d02c42694d017f09f83ca1fa2b9341c3
Aleksandr Mingalev <amingalev@gmail.com>
2025-01-21 00:19:41 +0100

    feat: complete task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir } = require('fs/promises');
+
+
+async function readDirectory() {
+  try {
+    const files = await readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true});
+    files.forEach(file => {
+      if (file.isFile()) {
+        const filename = file.name.split('.');
+        const ext =  (path.extname(file.name)).slice(1, path.extname(file.name).length);
+        const pathToCheck = path.join(__dirname, 'secret-folder', file.name);
+        fs.stat(pathToCheck, (err, stats) => {
+          if(!err){
+            console.log(filename[0] + " - " + ext + ' - ' + stats.size +' bytes');
+          }
+        });
+      }
+    });
+    } 
+    catch (err) {
+    console.log(err);
+  }
+}
+
+readDirectory();
