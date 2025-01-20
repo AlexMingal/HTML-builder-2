d389ce5272d78f0b21e653d4b78d2f0f2900598d
Aleksandr Mingalev <amingalev@gmail.com>
2025-01-20 04:07:45 +0100

    fix: correct closing writesream

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -10,18 +10,16 @@ const rl = readline.createInterface({
 // const filename = __dirname + 'text.txt';
 const ws = fs.createWriteStream(path.join(__dirname, 'text.txt'), {encoding: 'utf8', flags: 'a'});
 
-console.log('Input something');
+console.log('Input something:');
 
 rl.on('line', (input) => {
   if(input.toLowerCase() === 'exit') {
-    console.log('Finished!');
+    console.log('Ok, exiting!');
     rl.close();
     ws.end();
-  return;
+    return;
   }
-
-  console.log(`You wrote: ${input}`);
+  console.log(`You wrote: "${input}". \n Write something else!`);
   ws.write(input + '\n');
-
-  rl.on('close', ()=> {console.log('Thanks and Goodbye!');})
-})
+});
+  rl.on('close', () => console.log('File has been saved'));
