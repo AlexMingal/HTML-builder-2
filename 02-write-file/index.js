799e83018e1a78f9e025572fc9855fbcd6cf0777
Aleksandr Mingalev <amingalev@gmail.com>
2025-01-20 03:26:20 +0100

    feat: finish 02-write-file task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,4 +1,27 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
-ws = fs.createWriteStream(path.join(__dirname), 'info.txt'), {encoding: 'utf';}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// const filename = __dirname + 'text.txt';
+const ws = fs.createWriteStream(path.join(__dirname, 'text.txt'), {encoding: 'utf8', flags: 'a'});
+
+console.log('Input something');
+
+rl.on('line', (input) => {
+  if(input.toLowerCase() === 'exit') {
+    console.log('Finished!');
+    rl.close();
+    ws.end();
+  return;
+  }
+
+  console.log(`You wrote: ${input}`);
+  ws.write(input + '\n');
+
+  rl.on('close', ()=> {console.log('Thanks and Goodbye!');})
+})
